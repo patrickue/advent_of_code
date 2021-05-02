5d8b33e808afb5d230f9ff6a62cfce21df5b00f6
Patrick Thomas <patrick-thomas@gmx.net>
2021-05-02 17:38:04 +0200

    aoc2020-d07all

--- 2020/7_handy_haversacks/bag_checker/src/main.rs
+++ 2020/7_handy_haversacks/bag_checker/src/main.rs
@@ -14,7 +14,7 @@ enum ArgsError {
 #[derive(Debug)]
 struct BagRule {
     outside: String,
-    inside: Vec<String>
+    inside: Vec<(usize, String)>
 }
 
 fn main() {
@@ -32,9 +32,9 @@ fn main() {
             let bagrules = answer_vec.into_iter()
                 .map(|l| parse_rules(l))
                 .collect::<Vec<BagRule>>();
-            let outside_bags = get_all_bags_containing(bagrules, "shiny gold".to_string());
-            println!("Outside bags {:?}", outside_bags);
-            println!("Outside bag-len {:?}", outside_bags.len());
+            let outside_bags = find_bag_amount_inside(&bagrules, "shiny gold".to_string());
+            println!("Inside bag amount {:?}", outside_bags);
+            //println!("Outside bag-len {:?}", outside_bags.len());
         }
         Err(text) => println!("Error occured: {}", text),
     }
@@ -89,13 +89,14 @@ fn parse_rules<'a>(line: String) -> BagRule {
     }
 
     //Split second part & match
-    let mut inside_colors: Vec<String> = vec![];
+    let mut inside_colors: Vec<(usize, String)> = vec![];
     let second_parts: Vec<&str> = line_parts[1].split(", ").collect();
     for inside in second_parts {
-        let re2 = Regex::new(r"^\s*\d\s(\w+\s\w+)\sbags?[\s.,]{0,3}").unwrap();
+        let re2 = Regex::new(r"^\s*(\d)\s(\w+\s\w+)\sbags?[\s.,]{0,3}").unwrap();
         for inside_part in re2.captures_iter(inside) {
-            println!("Inside bag: {}", &inside_part[1]);
-            inside_colors.push(String::from(&inside_part[1]));
+            let amount = &inside_part[1].parse::<usize>().unwrap();
+            println!("Inside bag: {}x{}", amount, &inside_part[2]);
+            inside_colors.push((*amount, String::from(&inside_part[2])));
         }
     }
     if outside_color == "" {
@@ -109,35 +110,24 @@ fn parse_rules<'a>(line: String) -> BagRule {
     return returnbagrule;
 }
 
-fn get_all_bags_containing(bag_rules: Vec<BagRule>, insidesearch: String) -> Vec<String> {
-    let mut res_vec: Vec<String> = vec![];
-    let mut looking_for_vec: Vec<String> = vec![insidesearch];
-    let mut is_search = true;
-    while is_search {
-        if looking_for_vec.is_empty() {
-            is_search = false;
-        }
-        else {
-            let mut looking_for_bag = looking_for_vec.pop().unwrap();
+//fn find_outside_bags
 
-            for rule in &bag_rules {
-                if rule.inside.contains(&looking_for_bag) {
-                    println!("Bagrule: {:?}", rule);
-                    looking_for_vec.push(rule.outside.clone());
-                }
+fn find_bag_amount_inside (bag_rules: &Vec<BagRule>, search: String)
+    -> usize {
+
+    let mut bag_amount: usize = 0;
+    for rule in bag_rules {
+        if rule.outside == search {
+            if rule.inside.is_empty() {
+                //bag_amount = 1;
             }
-            if !res_vec.contains(&looking_for_bag) {
-                res_vec.push(looking_for_bag);
+            else {
+                for (amount, inside_bag) in &rule.inside {
+                    bag_amount += amount * (1 + find_bag_amount_inside(&bag_rules,
+                                                                  inside_bag.to_string()));
+                }
             }
         }
     }
-    return res_vec;
-}
-
-//fn find_outside_bags
-
-fn find_bag_amount_inside (bag_rules: Vec<BagRule>, insidesearch: String)
-    -> usize {
-
-
+    bag_amount
 }
