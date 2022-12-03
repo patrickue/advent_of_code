2806537a7db9821d6218a3b0e6d7e0ab59283153
Patrick Thomas <4194099+patrickue@users.noreply.github.com>
2022-12-03 21:30:22 +0100

    Small clean up

--- 2022/01_calorie_counting/main.py
+++ 2022/01_calorie_counting/main.py
@@ -1,9 +1,9 @@
 import typing
 
-# from dataclasses import dataclass
+from dataclasses import dataclass
 from pprint import pprint
 
-# from calorie_inventory import ElfInventoryParser
+from calorie_inventory import ElfInventoryParser
 from ports import InputPort
 from rock_paper_scissors import (
     RockPaperScissorsStrategyParser,
@@ -25,21 +25,44 @@ class FileReaderAdapter(InputPort):
         return self.file.__next__()
 
 
-def main() -> None:
+def main_01_calorie_counting() -> None:
+
+    calorie_inventory = ElfInventoryParser.parse(
+        input_reader=FileReaderAdapter("01_calorie_counting.txt")
+    )
+    # part 1
+    max_calories = max(calorie_inventory, key=lambda x: x.get_total_calories())
+    print(
+        f"Elf with the biggest inventory carries {max_calories.get_total_calories()} calories."
+    )
+
+    # part2
+    sorted_calorie_inventories = sorted(
+        calorie_inventory, key=lambda x: x.get_total_calories()
+    )
+    top_three_elves_calories = sum(
+        inventory.get_total_calories() for inventory in sorted_calorie_inventories[-3:]
+    )
+    print(f"Top three elves carry {top_three_elves_calories} calories.")
+
+
+def main_02_rock_paper_scissors() -> None:
 
     rps_strategy = RockPaperScissorsStrategyParser.parse(
         input_reader=FileReaderAdapter("02_paper_rock_scissors.txt")
     )
-    for rps_round in rps_strategy:
-        round_score = RockPaperScissorsScoreCalculator.calculate_score(rps_round)
-        pprint(f"Round: {rps_round}, scored: {round_score}")
+    # for rps_round in rps_strategy:
+    #    round_score = RockPaperScissorsScoreCalculator.calculate_score(rps_round)
+    #    pprint(f"Round: {rps_round}, scored: {round_score}")
 
+    # part 2
     total_strategy_score = sum(
         RockPaperScissorsScoreCalculator.calculate_score(rps_round)
         for rps_round in rps_strategy
     )
-    pprint(f"Total strategy score: {total_strategy_score}")
+    print(f"Total strategy score: {total_strategy_score}")
 
 
 if __name__ == "__main__":
-    main()
+    main_01_calorie_counting()
+    main_02_rock_paper_scissors()
