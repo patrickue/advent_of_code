c5ebedca1cffc6f42fbc5243e29032171e982bf7
Patrick Thomas <4194099+patrickue@users.noreply.github.com>
2022-12-07 21:36:30 +0100

    day 7 initial

---
+++ 2022/aoc07_no_space_left_python/main.py
@@ -0,0 +1,34 @@
+import re
+from pprint import pprint
+
+
+def parse_input(file, name) -> (int, int):
+    size = 0
+    small_folder_size = 0
+    while True:
+        line = file.readline()
+        if line == "":
+            return small_folder_size, size
+        extracted_filesize = re.search(r"(\d+) [a-zA-Z.]+", line)
+        if extracted_filesize is not None:
+            size += int(extracted_filesize.group(1))
+        elif line.startswith("$ cd .."):
+            pprint(f"Closed folder {name}:{size}")
+            return small_folder_size, size
+        elif line.startswith("$ cd"):
+            (subsmall_folder_size, subfolder_size) = parse_input(file, name+'/'+line.strip()[5:])
+            size += subfolder_size
+            small_folder_size += subsmall_folder_size
+            if subfolder_size <= 100_000:
+                small_folder_size += subfolder_size
+
+        elif line.startswith("$ ls"):
+            pass
+        elif line.startswith("dir"):
+            pass
+
+
+file = open("input.txt", "r")
+
+pprint(f"Total {parse_input(file,'/')}")
+
