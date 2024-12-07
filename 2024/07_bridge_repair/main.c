68272f311351eb890d754258b3f7a806cca2bf38
patrickue <4194099+patrickue@users.noreply.github.com>
2024-12-07 18:43:07 +0100

    Holy fuck. "unsigned long long int". At least I learned something...

--- 2024/07_bridge_repair/main.c
+++ 2024/07_bridge_repair/main.c
@@ -1,11 +1,12 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>  // For strtol
 #include <string.h>  // For strtok
 
 
 typedef struct  {
-    int result;
-    int *ops;
+    unsigned long long int result;
+    unsigned long long int *ops;
     int size;
 } Equation;
 
@@ -18,7 +19,7 @@ void free_equation(Equation* eq) {
 
 Equation* parse_to_equation(char line[]) {
     char *token;
-    int value;
+    unsigned long long int value;
     Equation* eq = malloc(sizeof(Equation));
     eq->result = 0;
     eq->ops = NULL;
@@ -26,7 +27,7 @@ Equation* parse_to_equation(char line[]) {
 
     // Get the result first
     token = strtok(line, ": ");
-    value = strtol(token, NULL, 10);
+    value = strtoull(token, NULL, 10);
     // printf("Parsed result: %d from token %s\n", value, token);
 
     eq->result = value;
@@ -35,7 +36,7 @@ Equation* parse_to_equation(char line[]) {
     token = strtok(NULL, " ");
     while (token != NULL) {
         // Resize the array to hold one more integer
-        eq->ops = realloc(eq->ops, (eq->size + 2) * sizeof(int));
+        eq->ops = realloc(eq->ops, (eq->size + 2) * sizeof(unsigned long long int));
 
         // Check if realloc failed
         if (eq->ops == NULL) {
@@ -43,8 +44,7 @@ Equation* parse_to_equation(char line[]) {
             return NULL;
         }
 
-        // Convert the token to an integer and store it in the array
-        value = strtol(token, NULL, 10);
+        value = strtoull(token, NULL, 10);
         // printf("Parsed integer: %d from token %s\n", value, token);
 
         eq->ops[eq->size] = value;
@@ -55,6 +55,38 @@ Equation* parse_to_equation(char line[]) {
     }
     return eq;
 };
+
+bool try_operators(Equation* eq) {
+
+    // 2 ^ of eq->size in normal languages
+    unsigned int amount_of_trials = (2 << eq->size);
+    printf("Amount of trials: %d\n", amount_of_trials);
+
+    for (unsigned int trial = 0; trial < amount_of_trials; trial++)
+    {
+        // printf("\nTrial %d", trial);
+        unsigned long long int trial_res = eq->ops[0];
+        for (unsigned int i = 1; i < eq->size; i++)
+        {
+            if (((1 << i-1) & trial) != 0)
+            {
+                trial_res *= eq->ops[i];
+                // printf("Mul; ");
+            }
+            else
+            {
+                trial_res += eq->ops[i];
+                // printf("Add; ");
+            }
+        }
+
+        if (trial_res == eq->result){
+            return true;
+        }
+    }
+    return false;
+}
+
 
 int main() {
 
@@ -68,20 +100,27 @@ int main() {
 
     // Declare the variable for the data to be read from
     // file
-    char dataToBeRead[151];
+    char dataToBeRead[200];
 
-    while (fgets(dataToBeRead, 151, filePointer)
-           != NULL) {
-
+    unsigned long long int sum_of_test_values = 0;
+    int total_equations = 0;
+    while (fgets(dataToBeRead, 200, filePointer) != NULL)
+    {
+        total_equations++;
         // Print the dataToBeRead
         printf("%s", dataToBeRead);
         Equation* eq = parse_to_equation(dataToBeRead);
         if (eq != NULL) {
-            printf("Equation: res: %d first: %d size: %d\n", eq->result, eq->ops[1], eq->size);
+            printf("Equation: res: %llu first: %llu size: %d\n", eq->result, eq->ops[1], eq->size);
         }
-
+        if (try_operators(eq) != 0) {
+            sum_of_test_values += eq->result;
+        }
         free_equation(eq);
+        // break;
     }
+    printf("This many equations in total: %d\n", total_equations);
+    printf("This is the sum of test values for equations which work: %llu\n", sum_of_test_values);
     fclose(filePointer);
     return 1;
 };
